fb8b8c980f6f6ce02d7d677c0c3cd157087b8311
TechnoLukas <luklevus@outlook.com>
2023-06-02 14:08:10 +0200

    added tooltips and dropping down menu.

--- window.gd
+++ window.gd
@@ -6,6 +6,7 @@ var mousepos
 var mouseposold
 var dragging=false
 var menu = false
+var menuitems
 
 @export var imgname: String
 @export var imgpath: String
@@ -13,10 +14,33 @@ var menu = false
 
 func _process(delta):
 	mousepos=get_mouse_position()
+	menuitems=[$menuzone/List/Close,$menuzone/List/Save,$menuzone/List/Copy,$menuzone/List/Pin]
 	
 	if dragging:
 		position=Vector2(position)+mousepos-mouseposold
-
+	
+	if menu:
+		#$Menubg.get_theme_stylebox("StyleBoxFlat").bg_color=Color.
+		var new_style = load("res://menubg.tres")
+		var tween = create_tween()
+		tween.tween_property(new_style,"border_width_bottom",0,0.5).set_trans(Tween.TRANS_LINEAR)
+		$menuzone/menubg.add_theme_stylebox_override("normal", new_style)
+		new_style.resource_local_to_scene=true
+		for item in menuitems:
+			await get_tree().create_timer(0.1).timeout
+			item.visible=true
+	else:
+		var new_style = load("res://menubg.tres")
+		var tween = create_tween()
+		tween.tween_property(new_style,"border_width_bottom",230,0.5).set_trans(Tween.TRANS_LINEAR)
+		$menuzone/menubg.add_theme_stylebox_override("normal", new_style)
+		new_style.resource_local_to_scene=true
+		menuitems.reverse()
+		for item in menuitems:
+			await get_tree().create_timer(0.1).timeout
+			item.visible=false
+		
+		
 func _on_window_input(event):
 	if event is InputEventMouseButton:
 		
@@ -28,7 +52,6 @@ func _on_copy_pressed():
 	var output
 	
 	OS.execute("powershell.exe", ["-Command", "Get-ChildItem '"+imgpath+imgname+"' | Set-Clipboard"])
-	print(output)
 
 func _on_close_pressed():
 	DirAccess.remove_absolute(imgpath+imgname)
@@ -41,4 +64,4 @@ func _on_pin_pressed():
 	set("always_on_top",!always_on_top)
 
 func _on_menu_pressed():
-	pass # Replace with function body.
+	menu=!menu
